ad82be8019ae287623a7dcd992e3861412d29dd7
shubham12008 <80684205+shubham12008@users.noreply.github.com>
2021-09-01 18:55:58 +0530

    Update Block.js

--- Block.js
+++ Block.js
@@ -7,11 +7,26 @@ class Block{
       this.body = Bodies.rectangle(x, y, width, height, options);
       this.width = width;
       this.height = height;
+    this.Visibility = 255;
       World.add(world, this.body);
     }
     display(){
       var pos= this.body.position;
       rectMode(CENTER);
       rect(pos.x,pos.y,this.width, this.height);
-    }
-}
+      
+      
+  if(this.body.speed < 3){
+   rectMode(CENTER);
+   rect(pos.x,pos.y,this.width, this.height);
+  }
+  else{
+    World.remove(world,this.body);
+    push();
+    this.Visibility = this.Visibility - 5;
+    tint(255,this.Visibility);
+    rect(pos.x,pos.y,this.width, this.height);
+    pop();
+  }
+      
+    }}
